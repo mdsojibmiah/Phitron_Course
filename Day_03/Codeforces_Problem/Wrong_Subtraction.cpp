52e5b1b1e30e085b9afde0cbe06bbc248d2ec77c
mdsojibmiah <mdsojibmiah711@gmail.com>
2024-11-27 01:12:48 +0600

    Wrong_Subtraction Problem

---
+++ Day_03/Codeforces_Problem/Wrong_Subtraction.cpp
@@ -0,0 +1,27 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n,k,t;cin>>n>>k;
+//     for(int i=0;i<k;i++){
+//        t=(n%10==0)?n/10:n--;
+//         // if(n%10==0){
+//         //     n/=10;
+//         // }
+//         // else{
+//         //     n--;
+//         // }
+
+//     }
+//     cout << t;
+//     return 0;
+
+// }
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, k;
+    cin >> n >> k;
+    while (k--) n = (n % 10 == 0) ? n / 10 : n - 1;
+    cout << n;
+    return 0;
+}
