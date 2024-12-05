ce504853413b7d0e7dd8b79f1d395259aebfc1be
mdsojibmiah <mdsojibmiah711@gmail.com>
2024-12-05 09:27:00 +0600

    up

--- HackerRank_Problem/problem_3.cpp
+++ HackerRank_Problem/problem_3.cpp
@@ -1,7 +1,13 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    
+#include <stdio.h>
 
+int main() {
+    int N;
+    scanf("%d", &N);
+    for (int i = 1; i <= N; i++) {
+        for (int j = i; j >= 1; j--) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
     return 0;
 }
