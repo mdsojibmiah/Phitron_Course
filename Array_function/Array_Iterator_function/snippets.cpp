ce7590fcbdd591f81b506104d0db6df0d32133f7
mdsojibmiah <mdsojibmiah711@gmail.com>
2024-12-13 20:09:54 +0600

    snippets c++

---
+++ Array_function/Array_Iterator_function/snippets.cpp
@@ -0,0 +1,38 @@
+/* Author: Error_404_Coder
+   Email: mdsojibmiah.coder@gmail.com
+   Problem Link: 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// -------------------- Shortcuts --------------------
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define F first
+#define S second
+
+// -------------------- Constants --------------------
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+const ld PI = acos(-1.0);
+
+// -------------------- Input/Output Optimization --------------------
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+// -------------------- Main Function Template --------------------
+void solve() {
+    // Your code here
+}
+
+int main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
