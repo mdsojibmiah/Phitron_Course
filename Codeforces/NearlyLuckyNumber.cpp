cb129b9f17dfccca76602b800550dca8f6d5b28f
mdsojibmiah <mdsojibmiah711@gmail.com>
2025-01-07 21:35:50 +0600

    file create

---
+++ Codeforces/NearlyLuckyNumber.cpp
@@ -0,0 +1,35 @@
+/* Author: CodeWithError
+   Email: mdsojibmiah.coder@gmail.com
+   Date  : 01 / 07 / 2025
+   Time  : 21:35
+   Problem Link: https://codeforces.com/problemset/problem/110/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+const ld PI = acos(-1.0);
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void solve() {
+    // Your code here
+}
+
+int main() {
+    fast_io;
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
