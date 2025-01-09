8abe3ce1e9348827b23f4f11f5f34528f8c8f96d
mdsojibmiah <mdsojibmiah711@gmail.com>
2025-01-09 17:33:57 +0600

    file create

---
+++ Codeforces/FrequencyArray.cpp
@@ -0,0 +1,43 @@
+/* Author: CodeWithError
+   Email: mdsojibmiah.coder@gmail.com
+   Date  : 01 / 09 / 2025
+   Time  : 17:33
+   Problem Link: 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define YES cout << 'YES
+'
+#define Yes cout << 'Yes
+'
+#define NO cout << 'NO
+'
+#define No cout << 'No
+'
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+const ld PI = acos(-1.0);
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void solve() {
+    // Your code here
+}
+
+int main() {
+    fast_io;
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
