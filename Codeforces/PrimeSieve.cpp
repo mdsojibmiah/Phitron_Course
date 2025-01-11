da77567dd13b531d557518cfb5d742754a117f4b
mdsojibmiah <mdsojibmiah711@gmail.com>
2025-01-11 12:16:39 +0600

    dfdf

---
+++ Codeforces/PrimeSieve.cpp
@@ -0,0 +1,35 @@
+/* Author: CodeWithError
+   Email: mdsojibmiah.coder@gmail.com
+   Date  : 01 / 11 / 2025
+   Time  : 12:16
+   Problem Link: https://codeforces.com/group/yg7WhsFsAp/contest/355493/problem/P21
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+const ld PI = acos(-1.0);
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void solve() {
+    // Your code here
+}
+
+int main() {
+    fast_io;
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
