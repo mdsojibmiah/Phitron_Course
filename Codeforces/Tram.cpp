6b71617f68cf3a0413b3db62785fe3b50a1e363b
mdsojibmiah <mdsojibmiah711@gmail.com>
2025-02-09 12:40:34 +0600

    file update solve

---
+++ Codeforces/Tram.cpp
@@ -0,0 +1,43 @@
+/* Author: CodeWithError
+   Email: mdsojibmiah.coder@gmail.com
+   Date  : 02 / 09 / 2025
+   Time  : 12:39
+   Problem Link: https://codeforces.com/problemset/problem/116/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define YES cout << 'YES\n'
+#define Yes cout << 'Yes\n'
+#define NO cout << 'NO\n'
+#define No cout << 'No\n'
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void solve() {
+    int n, cur = 0, mx = 0;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        cur -= a;
+        cur += b;
+        mx = max(mx, cur);
+    }
+
+    cout << mx << endl;              
+}
+
+int main() {
+    fast_io;
+    solve();
+    return 0;
+}
