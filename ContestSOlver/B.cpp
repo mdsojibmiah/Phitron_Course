8c0895051e99a6c8f6061a290e594fd7a1a67cdb
mdsojibmiah <mdsojibmiah711@gmail.com>
2025-02-10 22:37:25 +0600

    file create

--- ContestSOlver/B.cpp
+++ ContestSOlver/B.cpp
@@ -1,46 +1,41 @@
-/* Author: CodeWithError
-   Email: mdsojibmiah.coder@gmail.com
-   Date  : 02 / 09 / 2025
-   Time  : 21:54
-   Problem Link: 
-*/
-
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-#define ll long long
-#define nl '\n'
-#define YES cout << 'YES\n'
-#define Yes cout << 'Yes\n'
-#define NO cout << 'NO\n'
-#define No cout << 'No\n'
-#define ld long double
-#define pb push_back
-#define all(v) v.begin(), v.end()
-
-
-#define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
 void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    
+    int b;
+    cin >> b; // যেহেতু m = 1, তাই একবারই ইনপুট নিতে হবে।
+    
+    for (int i = 0; i < n; i++) {
+        a[i] = b - a[i]; // নতুন ভ্যালু সেট করছি
+    }
+    
+    // চেক করবো নতুন অ্যারে sorted কিনা
+    for (int i = 1; i < n; i++) {
+        if (a[i] < a[i - 1]) {
+            cout << "NO\n";
+            return;
+        }
+    }
+    
+    cout << "YES\n";
+}
+
+int main() {
     int t;
     cin >> t;
     
     while (t--) {
-        string s;
-        cin >> s;
-
-        int len = s.length(), cnt = 0;
-
-        for (int i = 0; i < len - 1; i++) {
-            if (s[i] == s[i + 1]) cnt++;
-        }
-
-        cout << len - cnt << endl;
-    }              
-}
-
-int main() {
-    fast_io;
-    solve();
+        solve();
+    }
+    
     return 0;
 }
