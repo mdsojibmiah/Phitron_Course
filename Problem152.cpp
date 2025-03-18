28ea02cd3c34aff3822a22d1beb7d98b2c68bf30
mdsojibmiah <mdsojibmiah711@gmail.com>
2025-03-18 22:22:44 +0600

    link add

--- Problem152.cpp
+++ Problem152.cpp
@@ -2,7 +2,7 @@
    Email: mdsojibmiah.coder@gmail.com
    Date  : 03 / 18 / 2025
    Time  : 22:08
-   Problem Link: 
+   Problem Link: https://codeforces.com/problemset/problem/1742/A
 */
 
 #include <bits/stdc++.h>
