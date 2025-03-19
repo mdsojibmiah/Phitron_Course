dd30575d053bb4fcc1d8e100270b249df71f3562
mdsojibmiah <mdsojibmiah711@gmail.com>
2025-03-19 21:28:12 +0600

    link add

--- Problem337A.cpp
+++ Problem337A.cpp
@@ -2,7 +2,7 @@
    Email: mdsojibmiah.coder@gmail.com
    Date  : 03 / 19 / 2025
    Time  : 21:27
-   Problem Link: 
+   Problem Link: https://codeforces.com/problemset/problem/337/A
 */
 
 #include <bits/stdc++.h>
