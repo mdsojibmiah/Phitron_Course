d3af6c98a8314c492a654e111cb4f2eaffa9c850
mdsojibmiah <mdsojibmiah711@gmail.com>
2025-03-19 21:27:25 +0600

    change

---
+++ Problem337A.cpp
@@ -0,0 +1,36 @@
+/* Author: CodeWithError
+   Email: mdsojibmiah.coder@gmail.com
+   Date  : 03 / 19 / 2025
+   Time  : 21:27
+   Problem Link: 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define YES cout << 'YES\n'
+#define Yes cout << 'Yes\n'
+#define NO cout << 'NO\n'
+#define No cout << 'No\n'
+#define ld long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void solve() {
+              
+}
+
+int main() {
+    fast_io;
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
