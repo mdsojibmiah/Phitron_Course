554c98f727aec6ae1e6af7c6391ee4b999d8b22a
mdsojibmiah <mdsojibmiah711@gmail.com>
2025-03-21 21:05:00 +0600

    Link add

--- Problem581A.cpp
+++ Problem581A.cpp
@@ -2,7 +2,7 @@
    Email: mdsojibmiah.coder@gmail.com
    Date  : 03 / 21 / 2025
    Time  : 21:04
-   Problem Link: 
+   Problem Link: https://codeforces.com/problemset/problem/581/A
 */
 
 #include <bits/stdc++.h>
